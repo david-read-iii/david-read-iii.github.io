dcb6c53d70d4171c83b5badffd6ae5b973c9df4f
David Read <david-read-iii@users.noreply.github.com>
2025-06-04 22:35:13 -0400

    Move button behavior handler in common.js and put in each html file.

--- js/projects.js
+++ js/projects.js
@@ -232,10 +232,4 @@ document.addEventListener("DOMContentLoaded", function () {
         const carouselInner = seeMoreModal.querySelector("#carouselInner");
         setCarouselIndicatorText(carouselIndicatorText, carouselInner);
     });
-
-    // TODO: Consider putting this on every page with buttons (all of them).
-    // Removes default button behavior that makes them look active after clicking a button. 
-    document.querySelectorAll('.btn').forEach(button => {
-        button.addEventListener('mouseup', () => button.blur());
-    });
-});
+});x
