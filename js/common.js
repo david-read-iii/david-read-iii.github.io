a320437c59e6bdf3fec5755ad5fa93eba91e815a
David Read <david-read-iii@users.noreply.github.com>
2025-06-20 22:21:25 -0400

    Add tooltip text to buttons in the footer.

--- js/common.js
+++ js/common.js
@@ -1,8 +1,14 @@
-// Remove default focus styling from buttons after click.
 window.addEventListener('DOMContentLoaded', () => {
+  // Remove default focus styling from buttons after click.
   document.addEventListener('mouseup', event => {
     if (event.target.classList.contains('btn')) {
       event.target.blur();
     }
   });
+
+  // Enable tooltips.
+  const tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+  const tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
+  })
 });
